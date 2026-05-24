a32b04149d8021d5c0b0b5ffe1b4d23af8c8b63f
akash-2911 <2911akashpatel@gmail.com>
2026-05-23 22:11:36 -0600

    new ux

---
+++ app/src/main/java/com/focusfarm/service/SleepTracker.java
@@ -0,0 +1,16 @@
+package com.focusfarm.service;
+
+import android.content.Context;
+
+/**
+ * Reads screen on/off times via UsageStatsManager and calculates sleep duration.
+ */
+public class SleepTracker {
+
+    public SleepTracker(Context context) {
+        // TODO: store ApplicationContext
+    }
+
+    // TODO: query screen interactive events for last night
+    // TODO: compute sleep duration and persist via SharedPrefsManager
+}
